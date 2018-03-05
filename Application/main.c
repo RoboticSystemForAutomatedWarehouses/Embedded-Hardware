1fc5f63fb6517f727012a9dd6db49ce71598cce5
Mahmoud Abdelbary <mahmoud.abelbaty@gmail.com>
2018-03-05 09:44:19 +0200

    change motor test to GPIO FUNCTIONS

--- Application/main.c
+++ Application/main.c
@@ -2,15 +2,30 @@
 
 
 int main(){
-  motor_init();
-
-
+  DDRC =0xFF;
+  GPIO_InitPort('C',0xFF);
   while (1) {
     //direction
-    PORTC |= (1<<PC0);
-    PORTC &= (~(1<<PC1));
+    GPIO_SetPin('C',0);
+    GPIO_ClearPin('C',1);
     //enable
-    PORTC |= (1<<PC2);
+    GPIO_SetPin('C',2);
   }
   return 0;
 }
+
+
+///motor test code
+/**
+int main(){
+  DDRC =0xFF;
+  while (1) {
+    //direction
+    PORTC |= (1<<PC0);
+    PORTC &= ~(1<<PC1);
+    //enable
+    PORTC |= (1<<PC2);
+
+  return 0;
+}
+**/
