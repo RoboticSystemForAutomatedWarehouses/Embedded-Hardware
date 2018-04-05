5cd25d78e5f90d410395041f782625a4e7f0bf7e
Hossam Elbahrawy <hossam_elbahrawy@hotmail.com>
2018-04-05 12:44:08 +0200

    added interrupt and ISR for UART0 and UART1

--- MCAL/uart.c
+++ MCAL/uart.c
@@ -6,15 +6,17 @@ void UART_INIT(uint8_t UART)
 	{
 	case UART0:
 		//Send the low 8 bits of the UBRR0
-		UBRR0L = (uint8_t)(BAUD_RATE);  
-		//Send the high 8 bits of the UBRR0  
+		UBRR0L = (uint8_t)(BAUD_RATE);
+		//Send the high 8 bits of the UBRR0
 		UBRR0H = (uint8_t)((BAUD_RATE)>>8);
 		//Enable transmit and receiver
 		UCSR0B |=((1<<RXEN0)|(1<<TXEN0));
-		//8-bit data frame, 1 stop bit, No parity, Asynchronous mode     
-		UCSR0B |=((1<<UCSZ01)|(1<<UCSZ00));     
+		//8-bit data frame, 1 stop bit, No parity, Asynchronous mode
+		UCSR0B |=((1<<UCSZ01)|(1<<UCSZ00));
+		//Enable Recive inpterrupt for UART0
+		UCSR0B |=(1<<RXCIE0);
 		break;
-		
+
 	case UART1:
 		//Send the low 8 bits of the UBRR0
 		UBRR1L = (uint8_t)(BAUD_RATE);
@@ -24,15 +26,17 @@ void UART_INIT(uint8_t UART)
 		UCSR1B |=((1<<RXEN1)|(1<<TXEN1));
 		//8-bit data frame, 1 stop bit, No parity, Asynchronous mode
 		UCSR1B |=((1<<UCSZ11)|(1<<UCSZ10));
+		//Enable Recive inpterrupt for UART1
+		UCSR1B |=(1<<RXCIE1);
 		break;
 	}
- 
+
 }
 
 
 
 void UART_Transmit(uint8_t data, uint8_t UART)
-{	
+{
 	switch (UART)
 	{
 	case UART0 :
@@ -46,13 +50,13 @@ void UART_Transmit(uint8_t data, uint8_t UART)
 		UDR1 =data;
 		break;
 	}
-	 
+
 }
 
 
 
 uint8_t UART_Read(uint8_t UART)
-{ 
+{
 	switch (UART)
 	{
 	case UART0:
@@ -65,7 +69,7 @@ uint8_t UART_Read(uint8_t UART)
 		while( !(UCSR1A & (1<<RXC1)) ) ;
 		return UDR1;
 		break;
-	}	
+	}
 }
 
 
@@ -86,4 +90,12 @@ void UART_receiveString(unsigned char *Str,uint8_t UART)
 		Str[i] = UART_Read(UART) ;
 	}
 	Str[i] = '\0';
+}
+
+ISR(USART1_RX_vect) {
+		//Here goes the ISR code of UART0
+}
+
+ISR(USART1_RX_vect) {
+		//Here goes the ISR code of UART1
 }
